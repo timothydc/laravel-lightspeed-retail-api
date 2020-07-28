f419c8282395f09ec07e4b2c15d0f332da09e307
timothy <timothy.decort@polaris-dc.com>
2020-07-28 21:49:45 +0200

    Add phpunit

---
+++ tests/TestCase.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace TimothyDC\LightspeedRetailApi\Tests;
+
+use Illuminate\Support\Facades\Schema;
+use Orchestra\Testbench\TestCase as Orchestra;
+use TimothyDC\LightspeedRetailApi\LightspeedRetailApiServiceProvider;
+
+class TestCase extends Orchestra
+{
+    public function setUp(): void
+    {
+        parent::setUp();
+    }
+
+    protected function getPackageProviders($app): array
+    {
+        return [
+            LightspeedRetailApiServiceProvider::class,
+        ];
+    }
+}
