fd8badf66188ee33dcb347cd4c6e45a975b272d9
Thomas Verhelst <15680337+TVke@users.noreply.github.com>
2023-02-28 19:06:16 +0100

    Add pagination attributes access (#29)
    * Add the ability to accept after and before
    
    As they are encode already there is no need to parse them again
    
    * Add 'getWithPagination' method to provide attribute data
    
    * Update the readme to explain the pagination system
    
    * Add an import statement where possible
    
    * Fix typos
    
    thanks @timothydc

--- src/Http/Controllers/SaveAccessTokenController.php
+++ src/Http/Controllers/SaveAccessTokenController.php
@@ -5,12 +5,13 @@ namespace TimothyDC\LightspeedRetailApi\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use TimothyDC\LightspeedRetailApi\Facades\LightspeedRetailApi;
 
 class SaveAccessTokenController extends Controller
 {
-    public function __invoke(Request $request): \Illuminate\Http\RedirectResponse
+    public function __invoke(Request $request): RedirectResponse
     {
         // save access token
         LightspeedRetailApi::api()->startUpClient($request->get('code'));
