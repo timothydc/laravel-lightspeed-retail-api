dfebf7ca23b7502921c514f9e83c8219bf82c974
tarektaher <tarektaher@live.fr>
2023-03-09 12:24:23 +0100

    Create ResourceImage.php

---
+++ src/Services/Lightspeed/ResourceImage.php
@@ -0,0 +1,21 @@
+<?php
+declare(strict_types=1);
+
+namespace TimothyDC\LightspeedRetailApi\Services\Lightspeed;
+
+use Illuminate\Support\Collection;
+use TimothyDC\LightspeedRetailApi\Exceptions\DuplicateResourceException;
+use TimothyDC\LightspeedRetailApi\Resource;
+
+class ResourceImage extends Resource
+{
+    public static string $resource = 'Image';
+    public string $primaryKey = 'imageID';
+
+    public static string $description = 'description';
+    public static string $filename = 'filename';
+    public static string $publicID = 'publicID';
+    public static string $itemID = 'itemID';
+    public static string $Item = 'Item';
+    public static string $ItemMatrix	 = 'ItemMatrix';
+}
